fb7da139a354080beacd7c8da3f3f89826c55bf3
Krupakoo <163434425+Krupakoo@users.noreply.github.com>
2025-11-17 12:23:45 +0100

    Add files via upload

---
+++ php_03_proste_szablony_kredyt/templates/bottom.php
@@ -0,0 +1,15 @@
+
+    </div>
+
+    <div class="footer l-box is-center">
+		<p>
+		<?php out($page_footer); if (!isset($page_footer)) {  ?>  <?php } ?>
+		</p>
+        
+	</div>
+
+</div>
+
+
+</body>
+</html>
